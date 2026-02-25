d45765c42cda62e625ddc54849b9e4969b2b7022
Thorrak <thorrak@excelinspace.com>
2026-02-25 09:15:58 -0500

    Utilize device server for testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,21 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import compression from 'vite-plugin-compression'
 
+const deviceServer = 'http://127.0.0.1:8080'
+
 export default defineConfig({
   plugins: [
     preact(),
     compression({ algorithm: 'gzip', verbose: false, deleteOriginFile: true })
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: deviceServer,
+        changeOrigin: true,
+      }
+    },
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
